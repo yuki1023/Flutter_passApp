031437aee87b2985130a3dbafeb5ebc0d9496220
樋口裕貴 <sai102343@gmail.com>
2021-07-06 16:19:04 +0900

    プロジェクト作成＆バータイトル変更

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  // アプリのUIを作成している部分
+  // ウィジェット＝部品=大文字
+  // プロパティ=小文字
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // 上のバーの部分
+      appBar: AppBar(
+        title: Text("パスワードポスト"),
+      ),
+      body: Center(
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// todo ホーム画面のタイトル変更
+// todo リストを表示
+// todo リスト間に区切り線を表示
+// todo リスト表示を動的に
+// todo フロートアクションボタンをタップ時にリストを一つ追加表示
+// todo 新しい画面を作成し、リストをタップしたときに遷移
+// todo 新しい画面のレイアウト作成
+// todo 新しい画面にリストからデータを引き継ぐ
+
+
